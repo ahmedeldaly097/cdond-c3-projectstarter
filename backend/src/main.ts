c8c791cc99fc52ce92bed771e9aeafb11c043bb1
ahmedeldaly097 <ahmedeldaly097@gmail.com>
2023-02-14 02:05:51 +0200

    Update files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -45,7 +45,7 @@ async function bootstrap() {
       }
     },
   };
-  app.use(cors(corsOptions));
+  app.use(cors({  "origin": "*", "methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"}));
   app.useGlobalFilters(new ErrorFilter());
   await app.listen(config.PORT);
   logger.log(`Listening on port ${config.PORT}.`);
