ee0f103181dd8671f412553a235e5b727c843b6f
ahmedeldaly097 <ahmedeldaly097@gmail.com>
2023-02-16 02:54:40 +0200

    cloudfront update

--- .circleci/files/deploy.sh
+++ .circleci/files/deploy.sh
@@ -1,5 +1,5 @@
 aws cloudformation deploy \
 --template-file cloudfront.yml \
 --stack-name InitialStack \
---parameter-overrides WorkflowID=eldaly1997 \
+--parameter-overrides WorkflowID=6524698888887777778855 \
 --region us-east-1
